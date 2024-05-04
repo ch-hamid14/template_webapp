7027e017ebe701b03ae5cf639f6b70c5a32cbe98
ch-hamid14 <hamidashraf3440@gmail.com>
2024-05-05 00:51:49 +0500

    project template setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography } from 'antd'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Typography className='text-red-600 text-[26px]'>
+      Testing
+    </Typography>
+    </>
+  )
+}
+
+export default App
